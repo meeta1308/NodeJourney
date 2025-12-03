7efbcb5266eacf345ec9614cef24ca97a3d3dd7f
Meeta Napheriya <meetanapheriya@gmail.com>
2025-12-03 11:32:08 +0530

    This is my first server

--- firstServer/index.js
+++ firstServer/index.js
@@ -5,95 +5,88 @@ const path = require("path");
 const PORT = 5500;
 http
   .createServer((req, res) => {
-    console.log("URL:", req.url);
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
-    if (req.url === "/") {
-      res.write(`
-      <html >
-        <title>Making a Myntra Navbar</title>
-        <navbar style=" display: flex;
-  justify-content: space-around;
-  align-items: center; height:45px; width:100%;background-color:rgba(34, 34, 31, 1) ">
-     <a href="/" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Home </a> 
-        <a href="/men" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Men </a> 
-        <a href="/women" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> 
-        Women </a> 
-        <a href="/kids" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Kids </a>
-        <a href="/carts" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Carts </a></navbar>
-        <h2 style=" display:block; text-align:center">Welcome to Myntra</h2>
-      </html>
-    `);
+    res.write(`
+     <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Myntra</title>
+    <style>
+      * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+      }
+      body {
+        width: 100vw;
+        height: 100vh;
+      }
+      navbar {
+        width: 100%;
+        height: 4.5rem;
+        display: flex;
+        align-items: center;
+        background-color: #262424;
+      }
+      ul {
+        width: 100%;
+        padding: 1.2rem;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+      }
+        ul li{
+         list-style: none;
+         }
+      ul li a {
+        font-size: 1.2rem;
+        color: #ffffdd;
+        text-decoration: none;
+      }
+    </style>
+  </head>
+  <body>
+    <navbar>
+      <ul>
+        <li><a href="/"> Home </a></li>
+        <li><a href="/men"> Men </a></li>
+        <li><a href="/women"> Women </a></li>
+        <li><a href="/kids"> Kids </a></li>
+        <li><a href="/carts"> Carts </a></li>
+      </ul>
+    </navbar>
+`);
 
-      return res.end();
+    if (req.url === "/") {
+      res.write(
+        `<h2 style="display: block; margin-top:3rem; text-align: center">Welcome! to Myntra home Section</h2>`
+      );
     } else if (req.url === "/men") {
-      res.write(`
-      <html >
-        <navbar style=" display: flex;
-  justify-content: space-around;
-  align-items: center; height:45px; width:100%;background-color:rgba(34, 34, 31, 1) ">
-     <a href="/" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Home </a> 
-        <a href="/men" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Men </a> 
-        <a href="/women" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> 
-        Women </a> 
-        <a href="/kids" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Kids </a>
-        <a href="/carts" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Carts </a></navbar>
-        <h2 style=" display:block; text-align:center">Welcome! to Men Section</h2>
-      </html>
-    `);
-      return res.end();
+      res.write(
+        `<h2 style="display: block; margin-top:3rem; text-align: center">Welcome! to Men Section</h2>`
+      );
     } else if (req.url === "/women") {
-      res.write(`
-      <html >
-        <navbar style=" display: flex;
-  justify-content: space-around;
-  align-items: center; height:45px; width:100%;background-color:rgba(34, 34, 31, 1) ">
-     <a href="/" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Home </a> 
-        <a href="/men" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Men </a> 
-        <a href="/women" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> 
-        Women </a> 
-        <a href="/kids" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Kids </a>
-        <a href="/carts" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Carts </a></navbar>
-        <h2 style=" display:block; text-align:center">Welcome! to Women Section</h2>
-      </html>
-    `);
-      return res.end();
+      res.write(
+        `<h2 style="display: block; margin-top:3rem; text-align: center">Welcome! to Women Section</h2>`
+      );
     } else if (req.url === "/kids") {
-      res.write(`
-      <html >
-        <navbar style=" display: flex;
-  justify-content: space-around;
-  align-items: center; height:45px; width:100%;background-color:rgba(34, 34, 31, 1) ">
-     <a href="/" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Home </a> 
-        <a href="/men" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Men </a> 
-        <a href="/women" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> 
-        Women </a> 
-        <a href="/kids" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Kids </a>
-        <a href="/carts" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Carts </a></navbar>
-        <h2 style=" display:block; text-align:center">Welcome! to Kids Section</h2>
-      </html>
-    `);
-      return res.end();
+      res.write(
+        `<h2 style="display: block; margin-top:3rem; text-align: center">Welcome! to kids Section</h2>`
+      );
     } else if (req.url === "/carts") {
-      res.write(`
-      <html >
-        <navbar style=" display: flex;
-  justify-content: space-around;
-  align-items: center; height:45px; width:100%;background-color:rgba(34, 34, 31, 1) ">
-     <a href="/" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Home </a> 
-        <a href="/men" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Men </a> 
-        <a href="/women" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> 
-        Women </a> 
-        <a href="/kids" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Kids </a>
-        <a href="/carts" style="text-decoration:none;  font-size:1rem; color:rgba(250, 250, 240, 1);"> Carts </a></navbar>
-        <h2 style=" display:block; text-align:center">Welcome! to Carts Section</h2>
-      </html>
-    `);
-      return res.end();
+      res.write(
+        `<h2 style="display: block; margin-top:3rem; text-align: center">Welcome! to carts Section</h2>`
+      );
     } else {
-      res.write(`Error 404: Page not found`);
-      res.end();
+      res.write(
+        `<h2 style="display: block; margin-top:3rem; text-align: center">Error 404: page not found</h2>`
+      );
     }
+
+    res.write(`</body></html>`);
+    return res.end();
   })
   .listen(PORT, (error) => {
     if (!error) {
